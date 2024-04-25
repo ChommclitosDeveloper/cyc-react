d10b869ef286ded9a839bf0fde38bb1ab3431bc9
YojansC27M <yojans555cardenas@gmail.com>
2024-04-24 22:42:05 -0500

    Configuracion del proyecto

---
+++ src/Routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+const Routes: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default Routes;
