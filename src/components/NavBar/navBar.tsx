47b367967dd31b0100ef5c108cec27a24d08770b
YojansC27M <yojans555cardenas@gmail.com>
2024-04-25 16:45:42 -0500

    NavBar Mejorado
    Termine el borrador del NavBar

--- src/components/NavBar/navBar.tsx
+++ src/components/NavBar/navBar.tsx
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import './NavBar.css'; // Estilos CSS para el NavBar
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faTimes,
+  faUser,
+  faUtensils,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
+import "./NavBar.css"; // Estilos CSS para el NavBar
 
 const NavBar: React.FC = () => {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -14,26 +20,31 @@ const NavBar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-      <div className="navbar-toggle" onClick={toggleSidebar}>
+        <div className="navbar-toggle" onClick={toggleSidebar}>
           <FontAwesomeIcon icon={showSidebar ? faTimes : faBars} />
         </div>
         <div className="navbar-logo">
           <Link to="/">Logo</Link>
         </div>
-        
       </div>
-      <div className={`sidebar ${showSidebar ? 'show' : ''}`}>
-      <button className="close-btn" onClick={toggleSidebar}>
+      <div className={`sidebar ${showSidebar ? "show" : ""}`}>
+        <button className="close-btn" onClick={toggleSidebar}>
           <FontAwesomeIcon icon={faTimes} />
         </button>
         <div className="user-info">
           <h2>Bienvenido, Usuario</h2>
-          <img src="imagen-de-perfil.jpg" alt="Avatar" className="avatar" />
+          <FontAwesomeIcon icon={faUser} size="5x" className="avatar" />
         </div>
         <ul className="sidebar-list">
-          <NavItem to="/editar-perfil" onClick={toggleSidebar}>Editar Perfil</NavItem>
-          <NavItem to="/recetas" onClick={toggleSidebar}>Mis Recetas</NavItem>
-          <NavItem to="/crear-receta" onClick={toggleSidebar}>Crear Receta</NavItem>
+          <NavItem to="/editar-perfil" icon={faUser} onClick={toggleSidebar}>
+            Editar Perfil
+          </NavItem>
+          <NavItem to="/recetas" icon={faUtensils}  onClick={toggleSidebar}>
+            Mis Recetas
+          </NavItem>
+          <NavItem to="/crear-receta" icon={faCog}  onClick={toggleSidebar}>
+            Crear Receta
+          </NavItem>
           {/* Agrega más elementos de navegación según sea necesario */}
         </ul>
       </div>
@@ -43,17 +54,20 @@ const NavBar: React.FC = () => {
 
 interface NavItemProps {
   to: string;
+  icon: any;
   onClick?: () => void;
   children: React.ReactNode;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ to, onClick, children }) => {
+const NavItem: React.FC<NavItemProps> = ({ to, icon, onClick, children }) => {
   return (
     <li className="sidebar-item">
-      <Link to={to} onClick={onClick}>{children}</Link>
+      <Link to={to} onClick={onClick}>
+        <FontAwesomeIcon icon={icon} className="sidebar-icon" />
+        {children}
+      </Link>
     </li>
   );
 };
 
 export default NavBar;
-
