55eee32947ffdeae31d021e44b0cef036e7631cb
ChommclitosDeveloper <santiagosl0302@gmail.com>
2024-05-15 16:52:12 -0500

    Valor nutricional
    Apartado para el valor nutricional.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Personalizadas from './components/personalizadas/Personalizadas';
 import Register from './components/register/Register';
 import Preparacion from './components/preparacion/Preparacion';
 import Formulario from './components/Formulario/formulario';
+import ValorNutricional from './components/valornutricional/ValorNutricional';
 
 const AppContent: React.FC = () => {
   const location = useLocation();
@@ -28,6 +29,7 @@ const AppContent: React.FC = () => {
         <Route path="/personalizadas" element={<Personalizadas/>} />
         <Route path="/preparacion" element={<Preparacion/>} />
         <Route path="/formulario" element={<Formulario/>} />
+        <Route path="/nutricion" element={<ValorNutricional/>} />
       </Routes>
       </div>
     </div>
