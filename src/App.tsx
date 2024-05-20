311922acf11709196ac65a9717f453ecdd73a68d
YojansC27M <yojans555cardenas@gmail.com>
2024-05-20 16:53:40 -0500

    Cambios responsives

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar/navBar';
 import Filtro from './components/Filtro/filtrocaribe';
@@ -17,36 +17,57 @@ import FiltroOrinoquia from './components/Filtro/FiltroOrinoquia';
 import FiltroInsular from './components/Filtro/FiltroInsular';
 import FiltroPacifica from './components/Filtro/FiltroPacifica';
 import FiltroAmazonica from './components/Filtro/FiltroAmazonica';
+import PersistentDrawerLeft from './components/NavBar/navBar';
 
 const AppContent: React.FC = () => {
   const location = useLocation();
   const hideNavBar = location.pathname === '/login' || location.pathname === '/register';
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const hideDrawer = location.pathname === '/login' || location.pathname === '/register';
 
   return (
-    <div className='padre'>
-      {!hideNavBar && <NavBar onSidebarToggle={setIsSidebarOpen} />}
-      <div className={`padres ${isSidebarOpen ? 'sidebar-open' : ''}`}>
-      <Routes>
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login />} />
-        <Route path='/' element={<Homepage/>}/>
-        <Route path="/regiones" element={<Regiones/>} />
-        <Route path="/filtro caribe" element={<Filtro/>} />
-        <Route path="/filtro andina" element={<FiltroAndina/>}/>
-        <Route path="/filtro orinoquia" element={<FiltroOrinoquia/>}/>
-        <Route path="/filtro amazonica" element={<FiltroAmazonica/>}/>
-        <Route path="/filtro insular" element={<FiltroInsular/>}/>
-        <Route path="/filtro pacifica" element={<FiltroPacifica/>}/>
-        <Route path="/preparacion" element={<Preparacion/>} />
-        <Route path="/nutricion" element={<ValorNutricional/>} />
-        <Route path="/reseña historica" element={<Reseña/>} />
-        <Route path="/personalizadas/*" element={<Personalizadas/>} />
-        <Route path="formulario" element={<Formulario />} />
-      </Routes>
-      </div>
+    <>
+      
+      {hideDrawer ? (
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/regiones" element={<Regiones />} />
+          <Route path="/filtro caribe" element={<Filtro />} />
+          <Route path="/filtro andina" element={<FiltroAndina />} />
+          <Route path="/filtro orinoquia" element={<FiltroOrinoquia />} />
+          <Route path="/filtro amazonica" element={<FiltroAmazonica />} />
+          <Route path="/filtro insular" element={<FiltroInsular />} />
+          <Route path="/filtro pacifica" element={<FiltroPacifica />} />
+          <Route path="/preparacion" element={<Preparacion />} />
+          <Route path="/nutricion" element={<ValorNutricional />} />
+          <Route path="/reseña historica" element={<Reseña />} />
+          <Route path="/personalizadas/*" element={<Personalizadas />} />
+          <Route path="formulario" element={<Formulario />} />
+        </Routes>
+      ) : (
+        <PersistentDrawerLeft>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/regiones" element={<Regiones />} />
+            <Route path="/filtro caribe" element={<Filtro />} />
+            <Route path="/filtro andina" element={<FiltroAndina />} />
+            <Route path="/filtro orinoquia" element={<FiltroOrinoquia />} />
+            <Route path="/filtro amazonica" element={<FiltroAmazonica />} />
+            <Route path="/filtro insular" element={<FiltroInsular />} />
+            <Route path="/filtro pacifica" element={<FiltroPacifica />} />
+            <Route path="/preparacion" element={<Preparacion />} />
+            <Route path="/nutricion" element={<ValorNutricional />} />
+            <Route path="/reseña historica" element={<Reseña />} />
+            <Route path="/personalizadas/*" element={<Personalizadas />} />
+            <Route path="formulario" element={<Formulario />} />
+          </Routes>
+        </PersistentDrawerLeft>
+      )}
       {!hideNavBar && <FloatingButton />} {/* Añadir el botón flotante */}
-    </div>
+    </>
   );
 };
 
@@ -59,3 +80,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
-import './Homepage.css'
-import Slider from 'react-slick';
+import "./Homepage.css";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Grid } from "@mui/material";
 
 const Homepage = () => {
-
   const images = [
     "/crear_recetas/personalizadas1.jpeg",
     "/crear_recetas/personalizadas2.jpg",
@@ -17,7 +17,7 @@ const Homepage = () => {
     "/visitar_regiones/regiones1.jpg",
     "/visitar_regiones/regiones2.jpg",
     "/visitar_regiones/regiones3.webp",
-  ]
+  ];
 
   // Configuración del carrusel
   const settings = {
@@ -33,51 +33,68 @@ const Homepage = () => {
   const navigate = useNavigate(); // Hook for navigation
 
   const handleRegionesClick = () => {
-    navigate('/regiones'); // Navigate to register route on click
+    navigate("/regiones"); // Navigate to register route on click
   };
 
   const handlePersonalizadasClick = () => {
-    navigate('/personalizadas');
+    navigate("/personalizadas");
   };
 
   return (
-    
-    <div className='homepage'>
-
-      <header className="header">
-        <h1 className="title">Bienvenidos a RecetasCol</h1>
-        <p className="subtitle">Tu espacio favorito de recetas</p>
-      </header>
-
-      <div className="grid-container-homepage">
-
-        <button className="grid-item-1" onClick={handleRegionesClick}>
-          <div className="">
-            <h2>¡Entra y conoce las recetas que tenemos para ti!</h2>
-            <p>Puedes seleccionar una region de colombia para consultar las principales recetas que hay</p>
-            <Slider {...settings}>
-              {images2.map((image, index) => (
-              <img key={index} src={image} alt={`Imagen ${index + 1}`} />
-              ))}
-            </Slider>
-          </div>
-        </button>
-        
-        <button className="grid-item-2" onClick={handlePersonalizadasClick}>
-          <div className="">
-            <h2>Recetas personalizadas</h2>
-            <p>¡Puedes crear tus propias recetas aqui!</p>
-            <Slider {...settings}>
-              {images.map((image, index) => (
-              <img key={index} src={image} alt={`Imagen ${index + 1}`} />
-              ))}
-            </Slider>
-          </div>
-        </button>
-
-      </div>
+    <div className="homepage">
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid item xs={12}>
+          <header className="header">
+            <h1 className="title">Bienvenidos a RecetasCol</h1>
+            <p className="subtitle">Tu espacio favorito de recetas</p>
+          </header>
+        </Grid>
+        <Grid item xs={6}>
+          <Grid
+            container
+            spacing={2}
+            direction="row-reverse"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item xs={12}>
+              <button className="grid-item-1" onClick={handleRegionesClick}>
+                <div className="">
+                  <h2>¡Entra y conoce las recetas que tenemos para ti!</h2>
+                  <p>
+                    Puedes seleccionar una region de colombia para consultar las
+                    principales recetas que hay
+                  </p>
+                  <Slider {...settings}>
+                    {images2.map((image, index) => (
+                      <img
+                        key={index}
+                        src={image}
+                        alt={`Imagen ${index + 1}`}
+                      />
+                    ))}
+                  </Slider>
+                </div>
+              </button>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <button className="grid-item-2" onClick={handlePersonalizadasClick}>
+            <div className="">
+              <h2>Recetas personalizadas</h2>
+              <p>¡Puedes crear tus propias recetas aqui!</p>
+              <Slider {...settings}>
+                {images.map((image, index) => (
+                  <img key={index} src={image} alt={`Imagen ${index + 1}`} />
+                ))}
+              </Slider>
+            </div>
+          </button>
+        </Grid>
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
